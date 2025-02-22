fe8785e4697b0b557b74e0a0108cb80eadb0d5ba
Florian Mimmler <if23b241@technikum-wien.at>
2025-02-22 15:46:47 +0100

    initial basic layout, added resource dictionary, and data view

---
+++ TourPlanner/View/MainView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TourPlanner.BusinessLayer.ViewModel;
+
+namespace TourPlanner.View
+{
+    /// <summary>
+    /// Interaktionslogik für MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void OnTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox box)
+            {
+                if (string.IsNullOrEmpty(box.Text))
+                    box.Background = (ImageBrush)FindResource("search_icon");
+                else
+                    box.Background = null;
+            }
+        }
+    }
+}
