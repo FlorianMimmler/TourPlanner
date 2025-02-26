224dd598b2f66fef86534374c5227053ec24976e
Florian Mimmler <if23b241@technikum-wien.at>
2025-02-26 15:48:18 +0100

    adds viewmodel and basic layout for main content general tab

---
+++ TourPlanner/BusinessLayer/ViewModel/GeneralViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using TourPlanner.BusinessLayer.Model;
+using System.Diagnostics.Metrics;
+using System.Windows;
+using TourPlanner.View.Subviews.Maincontent;
+
+namespace TourPlanner.BusinessLayer.ViewModel
+{
+    class GeneralViewModel : DependencyObject
+    {
+
+        public static readonly DependencyProperty TourProperty =
+            DependencyProperty.Register("Tour", typeof(Tour), typeof(GeneralView), new PropertyMetadata(null));
+
+        public Tour Tour
+        {
+            get { return (Tour)GetValue(TourProperty); }
+            set
+            {
+                SetValue(TourProperty, value);
+            }
+        }
+
+        public GeneralViewModel()
+        {
+            // Sample data for Tour Logs
+            Tour = new() { Id = 1, Name = "Wienerwald" };
+
+        }
+
+
+    }
+}
--- TourPlanner/View/Subviews/Maincontent/GeneralView.xaml.cs
+++ TourPlanner/View/Subviews/Maincontent/GeneralView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TourPlanner.BusinessLayer.ViewModel;
 
 namespace TourPlanner.View.Subviews.Maincontent
 {
@@ -23,6 +24,7 @@ namespace TourPlanner.View.Subviews.Maincontent
         public GeneralView()
         {
             InitializeComponent();
+            DataContext = new GeneralViewModel();
         }
     }
 }
